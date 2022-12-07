48e2cfd370120451bc31338a18079a0d823e5a15
ThaiTran31 <thaitrancs31@gmail.com>
2022-12-07 17:35:48 +0700

    Create models of movies module in SQLite

---
+++ movies/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+class Director(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    origin_name = models.CharField(max_length=255, null=True, blank=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    TYPE_CHOICES = [
+        ('si', 'Single'),
+        ('se', 'Series'),
+    ]
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='si', null=True, blank=True)
+    STATUS_CHOICES = [
+        ('t', 'Trailer'),
+        ('o', 'Ongoing'),
+        ('c', 'Completed')
+    ]
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='t', null=True, blank=True)
+    # single type -> duration: {duration} minutes
+    # series type -> duration: {duration} minutes per a episode
+    duration = models.DecimalField(max_digits=3, decimal_places=0, null=True, blank=True)
+    on_theater = models.BooleanField(default=True, null=True, blank=True)
+    year = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    # Vietsub or Narration or both
+    lang = models.CharField(max_length=100, default='Vietsub', null=True, blank=True)
+    current_episode = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    episode_total = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    trailer_url = models.CharField(max_length=511, null=True, blank=True)
+    thumbnail_url = models.CharField(max_length=511, null=True, blank=True)
+    poster_url = models.CharField(max_length=511, null=True, blank=True)
+    categories = models.ManyToManyField(Category, null=True, blank=True)
+    actors = models.ManyToManyField(Actor, null=True, blank=True)
+    directors = models.ManyToManyField(Director, null=True, blank=True)
+
+class Episode(models.Model):
+    name = models.CharField(max_length=4, null=True, blank=True)
+    slug = models.SlugField(max_length=4, null=True, blank=True)
+    filename = models.CharField(max_length=255, null=True, blank=True)
+    embedded_link = models.CharField(max_length=511, null=True, blank=True)
+    m3u8_link = models.CharField(max_length=511, null=True, blank=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
